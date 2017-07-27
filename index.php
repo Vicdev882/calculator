592c606496805f62059b319e9d3d148a73f27cb8
Victor <victor@karotte>
2017-07-27 20:22:04 +0200

    [TASK]Use ComposerTrial v1.0

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: victor
+ * Date: 27.07.17
+ * Time: 20:12
+ */
+
+require __DIR__.'/vendor/autoload.php';
+
+$object2 = new \Vicdev882\Persons\Victor();
+echo $object2->sayGoodBye();
